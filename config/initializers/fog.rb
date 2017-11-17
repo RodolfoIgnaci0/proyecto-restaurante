9c6e8cbf5c30074e0f9f338e4b7717e11b98a0ca
gazzi <gazzi.andres@gmail.com>
2017-11-16 21:32:43 -0300

    algunos platos

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['AWS_KEY_RESTAURANTE'],                        # required
     aws_secret_access_key: ENV['AWS_SECRET_RESTAURANTE'],                        # required
-    #region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    region:                'us-east-2'                  # optional, defaults to 'us-east-1'
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
